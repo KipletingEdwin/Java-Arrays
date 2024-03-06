63d2f5bf461ef09e08d1c96bdd42b2ce434cb24b
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-03-06 12:13:01 +0300

    finalize the project

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,28 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //array = used to store multiple values in a single variable
+
+        String[] cars = {"Camaro", "Corvette", "Tesla", "BMW"};
+        cars[0] = "Mustang";
+//        System.out.println(cars[3]);
+
+
+        //Another way to describe Arrays
+
+        String[] myCars = new String[4];
+        myCars[0] = "Allion";
+        myCars[1] = "Premio";
+        myCars[2] = "Rush";
+        myCars[3] = "Fielder";
+
+//        System.out.println(myCars[3]);
+        for(int i=0; i<myCars.length; i++){
+            System.out.println(myCars[i]);
+
         }
+
+
     }
 }
